b2a8d064d992d3bbf7e7ae816d0343065e97f694
Willard Murray III <wbmurrayiii@icloud.com>
2021-08-22 20:17:14 -0700

    adding unit test for calculator
    Adds unit test for addition and subtraction

---
+++ test.calculator.py
@@ -0,0 +1,12 @@
+# This is unit test for testing basic calculator function 
+
+import calculator
+
+class TestCalc:
+
+    def test_addition(self):
+        assert == calculator.add(2,2)
+        
+    def test_subtraction(self):
+        assert == calculater.subtract(4,2)
+    
